9fd142694a4b991e1a1114d6ec7a94b3b3cb4f5f
Vik <jzenterprize90@gmail.com>
2021-06-18 23:20:31 -0400

    Indices and ranges in csharp

---
+++ IndicesRanges/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IndicesRanges
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IndicesAndRanges();
+
+        }
+        public static void IndicesAndRanges()
+        {
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            char lastElement = vowels[^1];   // u
+            char secondToLast = vowels[^2];  // o
+            char[] firstTwo = vowels[..2];   // a, e
+            char[] lastThree = vowels[..2];  // i, o, u
+            char[] middleOne = vowels[2..3]; // i
+            char[] lastTwo = vowels[^2..];   // o, u
+
+            Console.WriteLine(lastElement);
+            Console.WriteLine(secondToLast);
+            Console.WriteLine(firstTwo);
+            Console.WriteLine(lastThree);
+            Console.WriteLine(middleOne);
+            Console.WriteLine(lastTwo);
+        }
+    }
+}
